128fb3a24bad2188c8a017628fa75e0b60b293f9
dhenton <dhenton@dhenton>
2013-03-03 12:49:46 -0600

    added action builder demo using drag and drop from jquery ui

---
+++ selenium-sandbox/src/test/java/com/dhenton9000/selenium/sandbox/jquery/DraggableTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhenton9000.selenium.sandbox.jquery;
+
+import com.dhenton9000.selenium.wicket.WicketBy;
+import org.openqa.selenium.interactions.Action;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+// http://selenium.polteq.com/en/perform-a-sequence-of-actions-with-selenium-webdriver
+
+/**
+ *
+ * @author dhenton
+ */
+public class DraggableTest {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setEnableNativeEvents(true);
+        driver = new FirefoxDriver(profile);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void draggable() {
+       // http://jqueryui.com/resources/demos/draggable/default.html
+        driver.get("http://jqueryui.com/resources/demos/draggable/default.html");
+        WebElement draggable = driver.findElement(By.id("draggable"));
+        new Actions(driver).dragAndDropBy(draggable, 120, 120).build().perform();
+    }
+
+    @Test
+    public void droppable() {
+        driver.get("http://jqueryui.com/resources/demos/droppable/default.html");
+        WebElement draggable = driver.findElement(By.id("draggable"));
+        WebElement droppable = driver.findElement(By.id("droppable"));
+        new Actions(driver).dragAndDrop(draggable, droppable).build().perform();
+    }
+
+    @Test
+    public void selectMultiple() throws InterruptedException {
+        driver.get("http://jqueryui.com/resources/demos/selectable/default.html");
+        List<WebElement> listItems = driver.findElements(By.cssSelector("ol#selectable *"));
+        Actions builder = new Actions(driver);
+        builder.clickAndHold(listItems.get(1)).clickAndHold(listItems.get(2)).click();
+        Action selectMultiple = builder.build();
+        selectMultiple.perform();
+    }
+
+    @Test
+    public void sliding() {
+        driver.get("http://jqueryui.com/resources/demos/slider/default.html");
+        WebElement draggable = driver.findElement(By.className("ui-slider-handle"));
+        new Actions(driver).dragAndDropBy(draggable, 120, 0).build().perform();
+    }
+}
